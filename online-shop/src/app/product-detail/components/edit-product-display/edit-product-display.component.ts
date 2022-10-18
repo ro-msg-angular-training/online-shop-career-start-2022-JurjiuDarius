82577e676708599c26585c77d787746a2328b668
Darius Jurjiu-Tandrau <Darius.Jurjiu-Tandrau@msg.group>
2022-10-18 15:58:30 +0300

    Added Material and internationalization support to the project

--- online-shop/src/app/product-detail/components/edit-product-display/edit-product-display.component.ts
+++ online-shop/src/app/product-detail/components/edit-product-display/edit-product-display.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Product } from '../classes/product';
+import { Product } from '../../../classes/product';
 
 @Component({
   selector: 'app-edit-product-display',
